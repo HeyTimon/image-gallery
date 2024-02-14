07fb4f9ccbc837bc3d2c6d1b64399aa3cd2e7d70
HeyTimon <yohanestam@yahoo.com>
2024-02-14 23:22:13 +0700

    menambahkan file next.js img-gallery

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import Navbar from "./components/Navbar";
+
+export const revalidate = 3600
+
+export const metadata: Metadata = {
+  title: "Next.JS Image Gallery",
+  description: "Tut by DG",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main className="max-w-6xl mx-auto">
+          {children}
+          </main>
+      </body>
+    </html>
+  );
+}
